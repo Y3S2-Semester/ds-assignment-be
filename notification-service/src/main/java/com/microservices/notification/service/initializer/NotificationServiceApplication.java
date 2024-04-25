5dfb4c14153c15f3680ea8210c1a195cbf8bf18a
saputhebeast <sapumalwijekoon4@gmail.com>
2024-04-25 21:58:46 +0530

    Feat: formatting

--- notification-service/src/main/java/com/microservices/notification/service/initializer/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/microservices/notification/service/initializer/NotificationServiceApplication.java
@@ -1,9 +1,6 @@
 package com.microservices.notification.service.initializer;
 
 
-import com.microservices.notification.service.components.TextMessageSender;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
