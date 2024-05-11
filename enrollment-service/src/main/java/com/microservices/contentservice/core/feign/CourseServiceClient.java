b66abea98c00dceb3e38c7fe6e30fb25e40a6aad
saputhebeast <sapumalwijekoon4@gmail.com>
2024-05-11 13:40:08 +0530

    test: inter service communication

--- enrollment-service/src/main/java/com/microservices/contentservice/core/feign/CourseServiceClient.java
+++ enrollment-service/src/main/java/com/microservices/contentservice/core/feign/CourseServiceClient.java
@@ -6,7 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "course-service", url = "${course-service:http://localhost}:8100")
+//@FeignClient(name = "course-service", url = "${course-service:http://localhost}:8100")
+@FeignClient(name = "course-service")
 public interface CourseServiceClient {
 
     @GetMapping("/api/v1/course/{id}")
--- enrollment-service/src/main/java/com/microservices/contentservice/core/feign/UserServiceClient.java
+++ enrollment-service/src/main/java/com/microservices/contentservice/core/feign/UserServiceClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${user-service:http://localhost}:8000")
+//@FeignClient(name = "user-service", url = "${user-service:http://localhost}:8000")
+@FeignClient(name = "user-service")
 public interface UserServiceClient {
 
     @GetMapping("/api/v1/user/{id}")
