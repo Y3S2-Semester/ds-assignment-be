6d3e15304fa97fdfd34d459d0b065b2056e7ce0d
saputhebeast <sapumalwijekoon4@gmail.com>
2024-05-11 14:12:05 +0530

    Fix: feign client

--- enrollment-service/src/main/java/com/microservices/contentservice/core/feign/UserServiceClient.java
+++ enrollment-service/src/main/java/com/microservices/contentservice/core/feign/UserServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "user-service", url = "${user-service:http://localhost}:8000")
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "http://user-service:80")
 public interface UserServiceClient {
 
     @GetMapping("/api/v1/user/{id}")
